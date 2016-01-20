5f3179bd09715e3c982bfb17190c0d4564b3561c
Ronoaldo JLP <ronoaldo@gmail.com>
2016-01-20 20:47:12 -0200

    Simplificando e melhorando o OCR e o squaddump.py
    O script que faz o dump das telas foi simplificado,
    removendo uma etapa desnecessária do começo, e informando
    algumas dicas para simplificar o uso.
    
    O script que faz o parser (OCR) dos dados da imagem
    foi modificado para obter informações adicionais,
    e alguns parametros foram ajustados para deixar
    a informação utilizada mais precisa em alguns "edge cases".
    
    Ainda não está perfeito, requer muito trabalho,
    mas já deu para criar uma planilha com os dados.

--- squaddump.py
+++ squaddump.py
@@ -1,13 +1,25 @@
+#!/usr/bin/env python
+"""
+This Python script is designed to be executed from Monkey Runner tool
+in the Android SDK.
+
+This script send touch events to your device and takes screenshots
+of your SWGOH squad, saving images at /tmp/swgoh-squad/ folder.
+"""
+
 import subprocess, os, sys, time, logging, signal
 from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
 
 os.putenv('LANG', 'C')
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 # Global reference to the device
 logging.info("Waiting for device ...")
 device = MonkeyRunner.waitForConnection()
 logging.info("Connected to device %s" % (device.getProperty('build.model')))
+
+# Folder used to save screenshots
+FOLDER = "/tmp/swgoh-squad/"
 
 def cleanup(signum, frame):
     signal.signal(signal.SIGINT, signal.getsignal(signal.SIGINT))
@@ -16,7 +28,7 @@ def cleanup(signum, frame):
 
 def snap(name, display=False):
     logging.debug('Taking screenshot of device ...')
-    f = '/tmp/squaddump-' + name + '.png'
+    f = FOLDER + name + '.png'
     device.takeSnapshot().writeToFile(f, 'png')
 
     if display:
@@ -33,35 +45,43 @@ def launch():
     device.startActivity(component=run)
 
 def touch(x, y):
-    logging.info("Sending touch event %d, %d", x, y)
+    logging.debug("Sending touch event %d, %d", x, y)
     device.touch(x, y, MonkeyDevice.DOWN_AND_UP)
     time.sleep(2)
 
 def main():
+    os.makedirs(FOLDER)
+    logging.info("Initializing the program....")
+    logging.info("*** Important! After you see a screenshot of your device, make sure the device")
+    logging.info("    is awake and at the home of the game. Then click on the screenshot window on the PC")
+    logging.info("    and press the 'q' key in your keyboard to start dumping the images.")
+
     launch()
-    snap('homescreen', True)
-    
+    snap('device-homescreen', True)
+
     # Open characters screen
     touch(105, 300)
-    snap('characters')
+    time.sleep(3)
     
     # Open first char
     touch(213, 386)
     time.sleep(1)
     # Print all chars
     for i in range(71):
-        logging.info(">>> Snapping character %d ...", i)
+        logging.info("*** Snapping character #%d ...", i)
+        logging.info("    Capturing basic info ...")
         snap('character-%02d-char' % i)
 
         touch(958, 1012)
+        logging.info("    Capturing statistics")
         snap('character-%02d-stat' % i)
         touch(1540, 117)
 
-        logging.info("<<< Moving to next one ...")
+        logging.info("    Done, moving to next one ...")
         touch(1217, 204)
 
     # TODO(ronoaldo): ocr all character screens to get the values we need
-    logging.info("Finished!")
+    logging.info("Finished! Now you can run the squadparse.sh script with Bash.")
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, cleanup)
--- squadparse.sh
+++ squadparse.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # This script parses a set of images extracted from the Game screen
-# and performs OCR on them using tesseract and gocr tools.
+# and performs OCR on them using tesseract-ocr tool.
 # 
 # It also extracts some nice thumbnails for the characters based on their
 # names and saves on the /tmp/heroes/thumbs and /tmp/heroes/shards folders
@@ -10,27 +10,50 @@
 
 # Global parameters for the OCR tools used
 export tess="tesseract stdin stdout -l por -psm 7"
-export gocr="gocr -p /tmp/gocrdb/ -f UTF8 -"
 
-# Output directories
-mkdir -p /tmp/heroes/{thumbs,shards}
+FOLDER="/tmp/swgoh-squad"
+
+# Output directories of thumbs and other output
+mkdir -p ${FOLDER}/heroes/{thumbs,shards}
+
+log() {
+	if [ x"$DEBUG" != x"" ] ; then
+		echo >&2 "[squaddump] $@"
+	fi
+}
+
+# Print header
+echo -e "power\tlevel\tstars\tgear\thealth\tshards1\tshards2\tname"
 
 # Loop over all characters ...
-for i in `seq 0 70`; do
+NUMBERS="$(seq 0 70)"
+if [ $# -ge 1 ] ; then
+	export NUMBERS="$@"
+fi
+for i in $NUMBERS ; do
+	log "Processing character #${i}"
 	# Setting up some global constants
-    char=`printf '/tmp/squaddump-character-%02d-char.png' $i`
-    stat=`printf '/tmp/squaddump-character-%02d-stat.png' $i`
-	star=`printf '/tmp/squaddump-character-%02d-star.png' $i`
+	char=`printf "${FOLDER}/character-%02d-char.png" $i`
+	stat=`printf "${FOLDER}/character-%02d-stat.png" $i`
+	star=`printf "${FOLDER}/character-%02d-star.png" $i`
+
+	if [ ! -f $char ] ; then
+		log "Skipping file $char - not found/unreadable ..."
+		continue
+	fi
 
 	# Parses the character level
-    level=$(convert -crop 43x39+102+875  $char - | $tess)
+	level=$(convert -crop 43x39+102+875  $char - | $tess)
 
 	# Parses the character power
-    power=$(convert -crop 82x40+1712+173 $char - | convert - -resize 800x600 pnm:- | $tess | sed 's/[^0-9]//g')
-    
+	power=$(convert -crop 82x40+1712+173 $char - |\
+		convert - -resize 800x600 pnm:- | $tess |\
+		sed 's/[^0-9]//g')
+	
 	# Parses and fixes the character name. This is the hardest part and the parameters
 	# bellow produced the best results possible.
-    name=$(convert -crop 586x44+348+91 -threshold 60% $stat - | convert - -resize 800x600 pnm:- | $tess)
+	name=$(convert -crop 586x44+348+91 -threshold 60% $stat - |\
+		convert - -resize 800x600 pnm:- | $tess)
 	name=$( echo "$name" | sed \
 		-e 's/Draide/Dróide/g' \
 		-e 's/\[IT-5555/CT-5555/g' \
@@ -44,23 +67,28 @@ for i in `seq 0 70`; do
 		-e 's/URORR/URoRR/g' )
 
 	# Parses the gear and fixes some weird symbols.
-    gear="I"
-    if [ $level -gt 1 ] ; then
-        gear=$(convert -crop 258x48+675+835 -threshold 60% $char pnm:- | $tess)
-		gear=$(echo "$gear" | sed -e 's/\\Í/VI/g')
-	gear=$(echo $gear | sed 's/\./. /g' | awk '{print $3}')
-    fi
+	gear="I"
+	if [ $level -gt 1 ] ; then
+		gear=$(convert $char -crop 258x54+675+835 -threshold 60% pnm:- | $tess | head)
+		gear=$(echo "$gear" | sed -e 's/\\Í/VI/g' -e 's/\./. /g' -e 's/l/I/g' | awk '{print $3}')
+	fi
 
 	# Parses the required and current shard count for the next promotion/activation.
-    shards=$(convert -crop 45x46+1776+678 -threshold 90% $char pnm:- | $gocr | tr 'O' '0' | sed 's/[^0-9]//g' )
-    myshards=$(convert -crop 40x45+1721+678 -threshold 90% $char pnm:- | $gocr | sed 's/[^0-9]//g')
+	shards="$(convert -crop 45x46+1776+678 $char -resize 800x600 - | $tess | head)"
+	myshards="$(convert -crop 40x45+1718+678 $char -blur 0.9 - | convert - -sharpen 0x12 - | $tess digits)"
+
+	# Parses the character health
+	health="$(convert -crop 227x88+626+676 $stat -resize 800x600 - | tesseract stdin stdout -psm 6 | head)"
+	health="$(echo "$health" | tr -d '(' | tr -d ')' | bc)"
+	
 
 	# Fetch star rating. Tricky, but works just fine.
 	# 1. We convert the image into black and white, after blurring/sharpenning,
-	#    so we have a temp file with white circles.
-	convert -crop 218x36+851+209 $char -blur 4x8 pnm:- | convert - -sharpen 0x12 -negate -threshold 15% -negate $star
+	#	so we have a temp file with white circles.
+	convert -crop 218x36+851+209 $char -blur 4x8 pnm:- |\
+		convert - -sharpen 0x12 -negate -threshold 15% -negate $star
 	# 2. We check the highest pixel that is white,
-	#    and identify what is the highest start active.
+	#	and identify what is the highest start active.
 	starcount=7
 	for x in 200 170 140 110 80 50 20 ; do
 		pixel=`convert $star -crop 1x1+$x+18 -depth 8 txt:- | tail -n 1 | awk '{print $3}'`
@@ -68,13 +96,12 @@ for i in `seq 0 70`; do
 		let starcount--
 	done
 
-	# TODO(ronoaldo): Export the health as well.
 	# Here we get all stats in CSV format
-    # printf "%6s;%3s;%2s;%2s;%3s;%s;%s\n" "${power}" "${level}" "${myshards}" "${shards}" "${gear}" "${starcount}" "${name}"
-    printf "%s\t%s\t%s\t%s\t%s\t%s\t%s\n" "${power}" "${level}" "${starcount}" "${gear}" "${myshards}" "${shards}" "${name}"
+	# printf "%6s;%3s;%2s;%2s;%3s;%s;%s\n" "${power}" "${level}" "${myshards}" "${shards}" "${gear}" "${starcount}" "${name}"
+	printf "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" "${power}" "${level}" "${starcount}" "${gear}" "${health}" "${myshards}" "${shards}" "${name}" 
 
 	# Finally, we crop the character pictures. 
-	convert -crop 317x596+802+246 $char "/tmp/heroes/thumbs/${name}.png"
-	convert -crop 88x88+1522+742  $char "/tmp/heroes/shards/${name}.png"
+	convert -crop 317x596+802+246 $char "${FOLDER}/heroes/thumbs/${name}.png"
+	convert -crop 88x88+1522+742  $char "${FOLDER}/heroes/shards/${name}.png"
 done
 
